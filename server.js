73bbcdd450dd444cd90d6dea77dd1c988622aa4d
Tristan Bond <tristanebond@gmail.com>
2020-12-14 19:38:45 -0800

    more heroku

--- server.js
+++ server.js
@@ -14,17 +14,17 @@ let waitList=[];
 
 app.get("/", function(req, res) {
 
-    res.sendFile(path.join(__dirname, "./assets/index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
   });
 
 app.get("/tables", function(req, res) {
 
-    res.sendFile(path.join(__dirname, "./assets/tables.html"));
+    res.sendFile(path.join(__dirname, "tables.html"));
   });
 
 app.get("/reserve", function(req, res) {
 
-    res.sendFile(path.join(__dirname, "./assets/reserve.html"));
+    res.sendFile(path.join(__dirname, "reserve.html"));
   });
 
 app.get("/api/tables",function(req,res){
