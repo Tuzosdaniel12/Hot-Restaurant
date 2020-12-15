c83ff122402f6e642ac9e85322cecbdead3ac99a
Tristan Bond <tristanebond@gmail.com>
2020-12-14 19:05:55 -0800

    html routes and json routes

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+let tables=[];
+let waitList=[];
+
 
 app.get("/", function(req, res) {
 
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
-  app.listen(PORT, function() {
+app.get("/tables", function(req, res) {
+
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+app.get("/reserve", function(req, res) {
+
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+
+app.get("/api/tables",function(req,res){
+    res.json(tables);
+  })
+
+app.get("/api/waitlist",function(req,res){
+    res.json(waitList);
+})
+
+app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
