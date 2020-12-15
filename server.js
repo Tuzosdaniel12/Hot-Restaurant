2e3f53e14a9a05f47083f7c6e24d11e9f51b1135
Tristan Bond <tristanebond@gmail.com>
2020-12-14 19:45:26 -0800

    fixed path routes

--- server.js
+++ server.js
@@ -14,17 +14,17 @@ let waitList=[];
 
 app.get("/", function(req, res) {
 
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
 
 app.get("/tables", function(req, res) {
 
-    res.sendFile(path.join(__dirname, "tables.html"));
+    res.sendFile(path.join(__dirname,"./public/tables.html"));
   });
 
 app.get("/reserve", function(req, res) {
 
-    res.sendFile(path.join(__dirname, "reserve.html"));
+    res.sendFile(path.join(__dirname, "./public/reserve.html"));
   });
 
 app.get("/api/tables",function(req,res){
